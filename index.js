c9dbf681256786e9583b2e4e52b99162b25a010a
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-08-28 14:03:43 -0600

    Made user prompt menu

--- index.js
+++ index.js
@@ -76,3 +76,24 @@ const promptManager = () => {
     })
 };
 
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select which option you would like to continue with:',
+            choices: ['add an engineer', 'add an intern', 'finish building my team']
+        }
+    ]).then(userChoice => {
+        switch (userChoice.menu) {
+            case "add an engineer":
+                promptEngineer();
+                break;
+            case "add an intern":
+                promptIntern();
+                break;
+            default:
+                buildTeam();
+        }
+    });
+};
