f141eb42d69842df71ea0db46375939554fa8ab9
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-01 14:48:00 -0600

    Completed engineer employee card

--- src/generate-site.js
+++ src/generate-site.js
@@ -22,4 +22,23 @@ const generateTeam = (team) => {
         `;
         html.push(managerHtml);
     }
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = `
+        <div class="card employee-card">
+        <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${engineer.name}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a></li>
+            </ul>
+        </div>
+    </div>
+        `;
+        html.push(engineerHtml);
+    }
 }
